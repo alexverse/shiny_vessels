cad720dcd2de04c8bc65981138244a1819b1b0e4
alexverse <a.kouretsis@yahoo.gr>
2021-05-06 10:47:21 +0000

    Delete rsync destination files

--- jobs/render.sh
+++ jobs/render.sh
@@ -8,4 +8,4 @@ fi
 
 Rscript -e "library(rmarkdown); rmarkdown::render('get_clean.Rmd')"
 ls | grep -vE '\.csv|\.html|\.png' > exclude.txt 
-rsync -vaz --exclude-from=exclude.txt --exclude=".*" "$PWD"/ $FOLDER
+rsync -vaz --exclude-from=exclude.txt --exclude=".*" "$PWD"/ $FOLDER --delete
