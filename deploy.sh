1926af73b43a80492dfa21debf66596814cbf3d7
alexverse <a.kouretsis@yahoo.gr>
2021-05-08 12:29:51 +0000

    Make executable add option to build

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,13 @@ then
   mkdir "$FOLDER"  
 fi
 
-cd jobs
-Rscript -e "library(rmarkdown); rmarkdown::render('get_clean.Rmd')"
-ls | grep -vE '\.csv|\.html|\.png' > exclude.txt 
-rsync -vaz --exclude-from=exclude.txt --exclude=".*" "$PWD"/ $FOLDER/results --delete
+if [ $# -eq 1 ]
+then
+  cd jobs
+  Rscript -e "library(rmarkdown); rmarkdown::render('get_clean.Rmd')"
+  ls | grep -vE '\.csv|\.html|\.png|\.txt' > .exclude.txt
+  rsync -vaz --exclude-from=.exclude.txt --exclude=".*" "$PWD"/ $FOLDER/results --delete
+  cd ..
+fi
 
-cd ..
 rsync -vaz shiny/ $FOLDER/app --delete
