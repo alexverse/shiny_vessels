6bc5bc007c01a5ffe4868ff330282bafa5497bf7
alexverse <a.kouretsis@yahoo.gr>
2021-05-09 20:11:52 +0000

    Job for timeseries

--- shiny/server.R
+++ shiny/server.R
@@ -10,27 +10,21 @@ server <- function(input, output, session) {
   c(filter_data, vessel_type, vessel_name) %<-% filterServer("v_data", vars_dt, vessels_poll)
   
   observeEvent(vessel_name(),{
-    
     req(vessel_name())
-    
-    get_note(filter_data(), vessel_name()) %>%
-      toast(
-        duration = 10, 
-        class = "teal raised", 
-        session = session,
-        toast_tags = list(
-          position = "bottom right",
-          showIcon = "water"
-        )
-      )
-    
+    get_toaster(filter_data(), vessel_name(), session)
   })
-
+  
+  observeEvent(filter_data(),{
+    req(nrow(filter_data()) == 0)
+    warn_toaster(session)
+  })
+  
   output$map <- renderLeaflet({
     
     on.exit(waiter_hide())
+
+    req(nrow(filter_data()) > 0) 
     
-    req(nrow(filter_data()) > 0)
     vessels_map(
       filter_data(), 
       all_vessels = is.null(vessel_name()),
@@ -42,9 +36,10 @@ server <- function(input, output, session) {
   output$data <- renderTable({
     
     dat <- filter_data()
-    if(!is.null(vessel_name())){
+    
+    if(!is.null(vessel_name()))
       dat <- nearest_vessels(dat, vessels_poll(), top = 6)
-    }
+    
     dat <- dat[order(-DIST), head(.SD, n_rows), .SDcols = render_cols]
     setnames(dat, render_cols_names)
     
---
+++ shiny/src/functions/get_toaster.R
@@ -0,0 +1,40 @@
+#' Toast 
+#'
+#' HTML generated to notify on selected vessel
+#'
+#' @param dat data.table 
+#' @param vessel_id 
+#'
+#' @return HTML
+#'
+#' @examples
+
+get_toaster <- function(dat, vessel_id, session){
+  
+  stopifnot(!is.reactive(dat))
+  stopifnot(!is.reactive(vessel_id))
+  
+  get_note(dat, vessel_id) %>%
+    toast(
+      duration = 10, 
+      class = "teal raised", 
+      session = session,
+      toast_tags = list(
+        position = "bottom right",
+        showIcon = "water"
+      )
+    )
+
+}
+
+warn_toaster <- function(session)
+  toast(
+    duration = 10, 
+    "No data found. Reset filters", 
+    class = "warning", 
+    session = session,
+    toast_tags = list(
+      position = "bottom right", 
+      showIcon = "exclamation triangle"
+    )
+  )
